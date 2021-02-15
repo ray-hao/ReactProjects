07817a7ef7d14ee787040b9f8999f55d342f9d75
Ray Hao <71276800+ray-hao@users.noreply.github.com>
2021-02-15 17:06:21 -0500

    Create App.js

---
+++ formUsingHooks/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from "react"
+import HookForm from './components/useStateWithObjects.js';
+
+function App() {
+  return (
+    <div className="App">
+      <HookForm />      
+    </div>
+  )
+}
+
+export default App;
