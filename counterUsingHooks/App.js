dc53ce72c60c4d26008b86627ffb97c140d524b9
Ray Hao <71276800+ray-hao@users.noreply.github.com>
2021-02-15 15:47:43 -0500

    Create App.js

---
+++ counterUsingHooks/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from "react"
+import ClassCounter from  "./components/ClassCounter.js"
+import HookCounter from './components/HookCounter';
+
+function App() {
+  return (
+    <div className="App">
+      <HookCounter />      
+    </div>
+  );
+}
+
+export default App;
