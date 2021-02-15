0203d887b9b71db0ab5cfe42b6ffe958eb0151fc
Ray Hao <71276800+ray-hao@users.noreply.github.com>
2021-02-15 18:16:04 -0500

    Create App.js

---
+++ listUsingHooks/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from "react"
+import HookArray from './components/useStateWithArrays';
+
+function App() {
+  return (
+    <div className="App">
+      <HookArray />      
+    </div>
+  )
+}
+
+export default App;
