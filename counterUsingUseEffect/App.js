7fdba790931fa1b41253cbba45c7e9a57a5844d0
Ray Hao <71276800+ray-hao@users.noreply.github.com>
2021-02-17 00:25:04 -0500

    Create App.js

---
+++ counterUsingUseEffect/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from "react"
+import CounterEffect from './components/counterUseEffect';
+
+function App() {
+  return (
+    <div className="App">
+      <CounterEffect />      
+    </div>
+  )
+}
+
+export default App;
