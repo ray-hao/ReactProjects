c5fa15f84f4a175f3db5e4ebcf552071ed0119bf
Ray Hao <71276800+ray-hao@users.noreply.github.com>
2021-02-17 00:58:42 -0500

    Create App.js

---
+++ runEffectsOnce/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from "react"
+import MouseLogger from './components/MouseLogger';
+
+function App() {
+  return (
+    <div className="App">
+      <MouseLogger />      
+    </div>
+  )
+}
+
+export default App;
