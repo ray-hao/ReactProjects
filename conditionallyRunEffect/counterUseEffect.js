d17b53cdaba17663019e6b0ab016331e04eafd39
Ray Hao <71276800+ray-hao@users.noreply.github.com>
2021-02-17 00:38:20 -0500

    Add files via upload

---
+++ conditionallyRunEffect/counterUseEffect.js
@@ -0,0 +1,24 @@
+import React, {useState, useEffect} from "react"
+
+function CounterEffect() {
+
+    const [count, setCount] = useState(0)
+    const [name, setName] = useState("")
+
+    useEffect(() => {
+        console.log("Updated Component")
+        document.title = `You clicked ${count} times`
+    }, [count])
+
+    return (
+        <div>
+            <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+            <button onClick={() => setCount(count + 1)}>
+                Clicked {count} Times
+            </button>
+
+        </div>
+    )
+}
+
+export default CounterEffect
