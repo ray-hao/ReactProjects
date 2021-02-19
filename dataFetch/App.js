f6306d47da7cb88093b1b30c73c9ada119fac4c9
Ray Hao <71276800+ray-hao@users.noreply.github.com>
2021-02-18 20:48:56 -0500

    Create App.js

---
+++ dataFetch/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from "react"
+import DataFetch from './components/DataFetch';
+
+function App() {
+  return (
+    <div className="App">
+      <DataFetch />      
+    </div>
+  )
+}
+
+export default App;
