b8f8dadb4ac034411de4daf776ebbb7017f36039
Ray Hao <71276800+ray-hao@users.noreply.github.com>
2021-03-03 00:28:02 -0500

    Create App.js

---
+++ randomReview/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from "react"
+import Practice from './components/Practice';
+
+function App() {
+  return (
+    <div className="App">
+      <Practice />      
+    </div>
+  )
+}
+
+export default App;
